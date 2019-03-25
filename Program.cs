38f950666aced825fb40023ad4e2ad788407b5cb
peemasit <peemasit@gmail.com>
2019-03-25 13:13:06 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsChallenge {
+    class Program {
+        enum Day { Mo, Tu, We, Th, Fr, Sa, Su }
+        enum Month { Jan = 1, Feb, Mar, Apr, May, Jun, Jul , Aug, Sep, Oct, Nov, Dec }
+        static void Main(string[] args) {
+            Month jan = Month.Jan;
+            Month may = Month.May;
+            Console.WriteLine(jan);
+            Console.WriteLine((int)may);
+            Console.ReadKey();
+        }
+    }
+}
